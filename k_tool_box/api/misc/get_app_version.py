db5a1f6a9167dec4aba269174ec5089995347697
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-11-06 04:42:01 +0800

    feat: add `get_app_version` API

---
+++ k_tool_box/api/misc/get_app_version.py
@@ -0,0 +1,23 @@
+from pydantic import RootModel
+
+from k_tool_box.api import BaseAPI, APIRet
+
+__all__ = ["GetAppVersion", "get_app_version"]
+
+
+class GetAppVersion(BaseAPI):
+    path = "/app_version"
+    method = "get"
+
+    class Response(RootModel[str]):
+        root: str
+
+    extra_validator = Response.model_validate_strings
+
+    @classmethod
+    async def __call__(cls) -> APIRet[str]:
+        return await cls.request()
+
+
+get_app_version = GetAppVersion.__call__
+"""Show current App commit hash"""
--- k_tool_box/cli.py
+++ k_tool_box/cli.py
@@ -1,9 +1,15 @@
+from k_tool_box.api.misc import get_app_version
 from k_tool_box.api.posts import get_creators
 
 __all__ = ["KToolBoxCli"]
 
 
 class KToolBoxCli:
+    @staticmethod
+    async def site_version():
+        """Show current Kemono site app commit hash"""
+        return await get_app_version()
+
     @staticmethod
     async def test():
         """run test"""
