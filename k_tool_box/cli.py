8ababe9d9f42faad1b8211b09fdc4d5f619b0da3
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-11-06 05:38:27 +0800

    feat: add `search_creator` command

--- k_tool_box/cli.py
+++ k_tool_box/cli.py
@@ -1,5 +1,7 @@
 from k_tool_box.api.misc import get_app_version
+from k_tool_box.api.model import Creator
 from k_tool_box.api.posts import get_creators
+from k_tool_box.enum import TextEnum
 
 __all__ = ["KToolBoxCli"]
 
@@ -7,8 +9,38 @@ __all__ = ["KToolBoxCli"]
 class KToolBoxCli:
     @staticmethod
     async def site_version():
+        # noinspection SpellCheckingInspection
         """Show current Kemono site app commit hash"""
-        return await get_app_version()
+        ret = await get_app_version()
+        return ret.data if ret else ret.message
+
+    # noinspection PyShadowingBuiltins
+    @staticmethod
+    async def search_creator(id: str = None, name: str = None, service: str = None):
+        """
+        Search creator, you can use multiple parameters.
+
+        :param id: The ID of the creator
+        :param name: The name of the creator
+        :param service: The service for the creator
+        """
+
+        async def inner(**kwargs):
+            def filter_func(creator: Creator):
+                for key, value in kwargs.items():
+                    if value is None:
+                        continue
+                    elif creator.__getattribute__(key) != value:
+                        return False
+                return True
+
+            ret = await get_creators()
+            if not ret:
+                return ret.message
+            creators = ret.data
+            return list(filter(filter_func, creators)) or TextEnum.SearchResultEmpty
+
+        return await inner(id=id, name=name, service=service)
 
     @staticmethod
     async def test():
---
+++ k_tool_box/enum.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+__all__ = ["TextEnum"]
+
+
+class TextEnum(StrEnum):
+    SearchResultEmpty = str(None)
