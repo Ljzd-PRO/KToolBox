d13e62a490d9a569b6e19abba6c7c32e2b429c48
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-11-07 19:23:07 +0800

    feat: enhanced downloader and runner

--- ktoolbox/downloader/downloader.py
+++ ktoolbox/downloader/downloader.py
@@ -1,76 +1,119 @@
+import asyncio
 import urllib.parse
 from pathlib import Path
+from typing import Callable, Any, Coroutine, Type
 
 import httpx
 import tqdm.asyncio
+from tqdm import tqdm as std_tqdm
 
 from ktoolbox.configuration import config
 from ktoolbox.downloader import DownloaderRet
 from ktoolbox.enum import RetCodeEnum
 
-__all__ = ["download"]
+__all__ = ["Downloader"]
 
 from ktoolbox.utils import file_name_from_headers
 
 
-async def download(
-        url: str,
-        path: Path,
-        *,
-        buffer_size: int = None,
-        chunk_size: int = None,
-        alt_filename: str = None,
-        progress: bool = False
-) -> DownloaderRet[str]:
-    """
-    Download a file from url to a destination path.
+class Downloader:
+    def __init__(
+            self,
+            url: str,
+            path: Path,
+            *,
+            buffer_size: int = None,
+            chunk_size: int = None,
+            alt_filename: str = None
+    ):
+        """
+        Initial a file downloader
 
-    :param url: Download URL
-    :param path: Directory path to save the file
-    :param buffer_size: Number of bytes for file I/O buffer
-    :param chunk_size: Number of bytes for chunk of download stream
-    :param alt_filename: Use this name if no filename given by the server
-    :param progress: Show progress bar
-    :return: `DownloaderRet` which contain the actual output filename
+        :param url: Download URL
+        :param path: Directory path to save the file
+        :param buffer_size: Number of bytes for file I/O buffer
+        :param chunk_size: Number of bytes for chunk of download stream
+        :param alt_filename: Use this name if no filename given by the server
 
-    About filename:
-        * If `Content-Disposition` is set in headers, use filename from it.
-        * Else if `alt_filename` parameter is set, use it.
-        * Else use filename from URL 'path' part.
-    """
-    buffer_size = buffer_size or config.downloader.buffer_size
-    chunk_size = chunk_size or config.downloader.chunk_size
-    async with httpx.AsyncClient() as client:
-        async with client.stream(
-                method="GET",
-                url=url,
-                follow_redirects=True,
-                timeout=config.downloader.timeout
-        ) as res:  # type: httpx.Response
-            if res.status_code != httpx.codes.OK:
-                return DownloaderRet(
-                    code=RetCodeEnum.GeneralFailure.value,
-                    message=f"Download failed - status_code: {res.status_code}"
-                )
-            if not (file_name := file_name_from_headers(res.headers)):
-                if not (file_name := alt_filename):
-                    file_name = urllib.parse.unquote(Path(url).name)
-            total_size = int(length_str) if (length_str := res.headers.get("Content-Length")) else None
-            with open(str(path / file_name), "wb", buffer_size) as f:
-                chunk_iterator = res.aiter_bytes(chunk_size)
-                t = tqdm.asyncio.tqdm(
-                    desc=file_name,
-                    total=total_size,
-                    disable=not progress,
-                    unit="B",
-                    unit_scale=True
-                )
-                async for chunk in chunk_iterator:
-                    f.write(chunk)
-                    t.update(len(chunk))  # Update progress bar
-    return DownloaderRet(
-        data=file_name
-    ) if file_name else DownloaderRet(
-        code=RetCodeEnum.GeneralFailure.value,
-        message="Download failed"
-    )
+        About filename:
+            * If `Content-Disposition` is set in headers, use filename from it.
+            * Else if `alt_filename` parameter is set, use it.
+            * Else use filename from URL 'path' part.
+        """
+
+        self.url = url
+        self.path = path
+        self.buffer_size = buffer_size or config.downloader.buffer_size
+        self.chunk_size = chunk_size or config.downloader.chunk_size
+        self.alt_filename = alt_filename
+
+        self.lock = asyncio.Lock()
+
+    @property
+    def finished(self) -> bool:
+        """
+        Check if the download finished
+
+        :return: `False` if the download **in process**, `False` otherwise
+        """
+        return not self.lock.locked()
+
+    async def run(
+            self,
+            *,
+            sync_callable: Callable[["Downloader"], Any] = None,
+            async_callable: Callable[["Downloader"], Coroutine] = None,
+            tqdm_class: Type[std_tqdm] = None,
+            progress: bool = False
+    ) -> DownloaderRet[str]:
+        """
+        Start to download
+
+        :param sync_callable: Sync callable for download finished
+        :param async_callable: Async callable for download finished
+        :param tqdm_class: `tqdm` class to replace default `tqdm.asyncio.tqdm`
+        :param progress: Show progress bar
+        :return: `DownloaderRet` which contain the actual output filename
+        """
+        tqdm_class: Type[std_tqdm] = tqdm_class or tqdm.asyncio.tqdm
+        async with self.lock:
+            async with httpx.AsyncClient() as client:
+                async with client.stream(
+                        method="GET",
+                        url=self.url,
+                        follow_redirects=True,
+                        timeout=config.downloader.timeout
+                ) as res:  # type: httpx.Response
+                    if res.status_code != httpx.codes.OK:
+                        return DownloaderRet(
+                            code=RetCodeEnum.GeneralFailure.value,
+                            message=f"Download failed - status_code: {res.status_code}"
+                        )
+                    if not (file_name := file_name_from_headers(res.headers)):
+                        if not (file_name := self.alt_filename):
+                            file_name = urllib.parse.unquote(Path(self.url).name)
+                    total_size = int(length_str) if (length_str := res.headers.get("Content-Length")) else None
+                    with open(str(self.path / file_name), "wb", self.buffer_size) as f:
+                        chunk_iterator = res.aiter_bytes(self.chunk_size)
+                        t = tqdm_class(
+                            desc=file_name,
+                            total=total_size,
+                            disable=not progress,
+                            unit="B",
+                            unit_scale=True
+                        )
+                        async for chunk in chunk_iterator:
+                            f.write(chunk)
+                            t.update(len(chunk))  # Update progress bar
+            if sync_callable:
+                sync_callable(self)
+            if async_callable:
+                await async_callable(self)
+            return DownloaderRet(
+                data=file_name
+            ) if file_name else DownloaderRet(
+                code=RetCodeEnum.GeneralFailure.value,
+                message="Download failed"
+            )
+
+    __call__ = run
--- ktoolbox/job/runner.py
+++ ktoolbox/job/runner.py
@@ -2,26 +2,40 @@ import asyncio
 from typing import List, Set
 from urllib.parse import urlunparse
 
+from tqdm import tqdm as std_tqdm
+
 from ktoolbox.configuration import config
-from ktoolbox.downloader import download
+from ktoolbox.downloader import Downloader
 from ktoolbox.job import Job
 
 __all__ = ["JobRunner"]
 
 
 class JobRunner:
-    def __init__(self, job_list: List[Job] = None):
+    def __init__(self, job_list: List[Job] = None, tqdm_class: std_tqdm = None):
         """
         Create a job runner
 
         :param job_list: Jobs to initial `self.job_queue`
+        :param tqdm_class: `tqdm` class to replace default `tqdm.asyncio.tqdm`
         """
         job_list = job_list or []
         self.job_queue: asyncio.Queue[Job] = asyncio.Queue()
         for job in job_list:
             self.job_queue.put_nowait(job)
+        self.tqdm_class = tqdm_class
         self.tasks: Set[asyncio.Task] = set()
+        self.downloaders: List[Downloader] = []
         self.lock = asyncio.Lock()
+
+    @property
+    def finished(self):
+        """
+        Check if all jobs finished
+
+        :return: `False` if **in process**, `False` otherwise
+        """
+        return not self.lock.locked()
 
     async def processor(self):
         """Process each job in `self.job_queue`"""
@@ -29,12 +43,18 @@ class JobRunner:
             job = await self.job_queue.get()
             url_parts = [config.downloader.scheme, config.api.attachment_netloc, job.server_path, '', '', '']
             url = urlunparse(url_parts)
-            await download(
+            downloader = Downloader(
                 url=url,
                 path=job.path
             )
+            self.downloaders.append(downloader)
+            await downloader.run(
+                sync_callable=self.downloaders.remove,
+                tqdm_class=self.tqdm_class
+            )
             # TODO: finished log
             self.job_queue.task_done()
+        await self.job_queue.join()
 
     async def start(self):
         """
