f525aba3dfb99d2e9fc7a6ecc1ae19a1f1da4b00
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-11-10 07:56:11 +0800

    fix: `Post.content` may be None

--- ktoolbox/action/job.py
+++ ktoolbox/action/job.py
@@ -59,7 +59,7 @@ async def create_job_from_post(
                 type=PostFileTypeEnum.File
             )
         )
-    if content_path:
+    if content_path and post.content:
         if not content_path.parent.is_dir():
             content_path.parent.mkdir()
         async with aiofiles.open(content_path, "w", encoding=config.downloader.encoding) as f:
