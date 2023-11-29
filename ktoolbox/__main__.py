a868fdc477455a990565d2f614c3b90bd8e26585
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-11-30 03:13:07 +0800

    build: add script in pyproject.toml

--- ktoolbox/__main__.py
+++ ktoolbox/__main__.py
@@ -3,6 +3,11 @@ import fire
 from ktoolbox.cli import KToolBoxCli
 from ktoolbox.utils import logger_init
 
-if __name__ == "__main__":
+
+def main():
     logger_init(cli_use=True)
     fire.Fire(KToolBoxCli)
+
+
+if __name__ == "__main__":
+    main()
