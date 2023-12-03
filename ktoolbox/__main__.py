bd4f256ae90d12996d5bae029d5faa146cc07b70
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-12-03 17:47:12 +0800

    feat: run `uvloop_init()` when using cli

--- ktoolbox/__main__.py
+++ ktoolbox/__main__.py
@@ -1,11 +1,12 @@
 import fire
 
 from ktoolbox.cli import KToolBoxCli
-from ktoolbox.utils import logger_init
+from ktoolbox.utils import logger_init, uvloop_init
 
 
 def main():
     logger_init(cli_use=True)
+    uvloop_init()
     fire.Fire(KToolBoxCli)
 
 
