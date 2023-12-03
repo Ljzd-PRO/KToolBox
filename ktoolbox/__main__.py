4f4079d549094e16da7acaea8e4c2a95bebee55d
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-12-03 18:56:48 +0800

    feat: output configuration to debug log

--- ktoolbox/__main__.py
+++ ktoolbox/__main__.py
@@ -1,11 +1,14 @@
 import fire
+from loguru import logger
 
 from ktoolbox.cli import KToolBoxCli
-from ktoolbox.utils import logger_init, uvloop_init
+from ktoolbox.configuration import config
+from ktoolbox.utils import logger_init, uvloop_init, generate_msg
 
 
 def main():
     logger_init(cli_use=True)
+    logger.debug(generate_msg(config=config))
     uvloop_init()
     fire.Fire(KToolBoxCli)
 
