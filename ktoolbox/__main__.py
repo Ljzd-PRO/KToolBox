4bcd7e115db49f81270022d4f2fde26dbd00e6b4
Ljzd-PRO <ljzd@office.ljzd-pro.ml>
2023-12-07 01:14:34 +0800

    feat: added `KeyboardInterrupt` catch

--- ktoolbox/__main__.py
+++ ktoolbox/__main__.py
@@ -7,10 +7,13 @@ from ktoolbox.utils import logger_init, uvloop_init, generate_msg
 
 
 def main():
-    logger_init(cli_use=True)
-    logger.debug(generate_msg(config=config))
-    uvloop_init()
-    fire.Fire(KToolBoxCli)
+    try:
+        logger_init(cli_use=True)
+        logger.debug(generate_msg(config=config))
+        uvloop_init()
+        fire.Fire(KToolBoxCli)
+    except KeyboardInterrupt:
+        logger.error("KToolBox was interrupted by the user")
 
 
 if __name__ == "__main__":
