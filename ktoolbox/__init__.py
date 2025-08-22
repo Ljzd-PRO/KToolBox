606b4e0d5f819b00b45b04958a236e5ed35a3239
Ljzd-PRO <me@ljzd.link>
2025-08-22 19:12:17 +0800

    build: bump version to v0.22.0

--- ktoolbox/__init__.py
+++ ktoolbox/__init__.py
@@ -1,4 +1,4 @@
 __title__ = "KToolBox"
 # noinspection SpellCheckingInspection
 __description__ = "A useful CLI tool for downloading posts in Kemono.cr / .su / .party"
-__version__ = "v0.21.1"
+__version__ = "v0.22.0"
